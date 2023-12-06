e883cc34c88711439db5715677d43d584384dc08
WireWhiz <eli.knisley@spatialape.com>
2023-12-06 14:21:08 -0700

    Remove debug logs

--- package/Scripts/Services/Implementation/FusionNetObjectAPI.cs
+++ package/Scripts/Services/Implementation/FusionNetObjectAPI.cs
@@ -97,10 +97,8 @@ namespace Foundry.Networking
         [Rpc(sources: RpcSources.All, targets: RpcTargets.StateAuthority, Channel = RpcChannel.Reliable, InvokeLocal = false)]
         void RPC_GetID(RpcInfo info = default)
         {
-            Debug.Log("Received RPC request from player " + info.Source + " for NetworkStateId");
             GetNetworkStateIdAsync(id =>
             {
-                Debug.Log("Sending RPC response to player " + info.Source + " with NetworkStateId " + id.Id + "");
                 RPC_SetId(info.Source, id.Id);
             });
         }
@@ -108,7 +106,6 @@ namespace Foundry.Networking
         [Rpc(sources: RpcSources.StateAuthority, targets: RpcTargets.All, Channel = RpcChannel.Reliable, InvokeLocal = false)]
         void RPC_SetId([RpcTarget] PlayerRef requestor, uint id)
         {
-            Debug.Log("Received RPC response containing network state id ");
             NetworkStateId = new(id);
         }
     }
